4b3c6d9da34daf4a5a1864c7a1dd92d71c536c9e
Tommy Lau <tommy@gen-new.com>
2015-02-10 17:50:19 +0800

    Enable iptables and ipv4 forwarding in container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -58,6 +58,18 @@ if [ ! -f /etc/ocserv/server-key.pem ] || [ ! -f /etc/ocserv/server-cert.pem ]; 
 	fi
 fi
 
+# Open ipv4 ip forward
+sysctl -w net.ipv4.ip_forward=1
+
+# Enable NAT forwarding
+iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+
+# Enable TUN device
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
+
 # Run OpennConnect Server
 exec "$@"
 
